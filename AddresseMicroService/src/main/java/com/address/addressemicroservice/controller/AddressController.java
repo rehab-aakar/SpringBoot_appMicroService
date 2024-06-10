3f479dd891453de32d85d44a2f8c6e2dd0d9a271
RehabDev <67470027+rehab-aakar@users.noreply.github.com>
2024-06-10 15:05:36 +0100

    add config to address controller

--- AddresseMicroService/src/main/java/com/address/addressemicroservice/controller/AddressController.java
+++ AddresseMicroService/src/main/java/com/address/addressemicroservice/controller/AddressController.java
@@ -4,6 +4,7 @@ package com.address.addressemicroservice.controller;
 import com.address.addressemicroservice.dto.AddressDto;
 import com.address.addressemicroservice.entity.Address;
 import com.address.addressemicroservice.service.AddressService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class AddressController {
 
     @Autowired
@@ -23,22 +25,28 @@ public class AddressController {
 
     @PostMapping("/address")
     public ResponseEntity<Object> addAddress(@RequestBody AddressDto addressDto) {
+        log.info("Add address");
         Address address = addressService.AddAddress(addressDto);
         if(address != null) {
+            log.info("Address added successfully");
             return new ResponseEntity<>(address, HttpStatus.CREATED);
         }
+        log.error("Address not added");
         return new ResponseEntity<>("Address not added",HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping("/address")
     public ResponseEntity<List<Address>> getAllAddresses() {
+        log.info("Get all addresses");
         List<Address> addressList = addressService.findAllAddresses();
         return new ResponseEntity<>(addressList, HttpStatus.OK);
     }
 
     @GetMapping("/address/{id}")
     public ResponseEntity<Object> getAddressById(@PathVariable int id) {
+        log.info("Get address by id");
         Optional<Address> address = addressService.findAddressById(id);
+
         return address.<ResponseEntity<Object>>map(
                         value -> new ResponseEntity<>(value, HttpStatus.OK)).
                 orElseGet(() -> new ResponseEntity<>("Address not found", HttpStatus.NOT_FOUND)
@@ -47,22 +55,27 @@ public class AddressController {
 
     @DeleteMapping("/address/{id}")
     public ResponseEntity<String> deleteAddressById(@PathVariable int id) {
+        log.info("Delete address by id");
         boolean isDeleted = addressService.deleteAddressById(id);
         String responseMessage = isDeleted ? "Address is deleted" : "Address not found";
         HttpStatus status = isDeleted ? HttpStatus.OK : HttpStatus.NOT_FOUND;
+        log.info(responseMessage);
         return new ResponseEntity<>(responseMessage, status);
     }
 
     @PutMapping("/address/{id}")
     public ResponseEntity<Object> updateAddressById(@PathVariable int id, @RequestBody AddressDto addressDto) {
         boolean isUpdated = addressService.updateAddress(id,addressDto);
+        log.info("Update address by id");
         Object responseMessage = null;
         if(isUpdated) {
+            log.info("Address updated successfully");
             Map<String,Object> map = new HashMap<>();
             map.put("Is updated",true);
             map.put("data",addressDto);
             responseMessage = map;
         }else{
+            log.error("Address not updated");
             responseMessage = "Address not found";
         }
         HttpStatus status = isUpdated ? HttpStatus.OK : HttpStatus.NOT_FOUND;
