b5c1cd14577c4fd9bf38ee9cd64f808b8aa65f5e
RehabDev <67470027+rehab-aakar@users.noreply.github.com>
2024-06-10 12:23:40 +0100

    first config addredd micro-service

---
+++ AddresseMicroService/src/main/java/com/address/addressemicroservice/AddresseMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.address.addressemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddresseMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddresseMicroServiceApplication.class, args);
+    }
+
+}
---
+++ AddresseMicroService/src/test/java/com/address/addressemicroservice/AddresseMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.address.addressemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddresseMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
